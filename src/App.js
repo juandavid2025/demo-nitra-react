eebd45b5badd02971acb152e520a157d516e7d77
Jose <joseossorio99@hotmail.com>
2021-03-16 15:16:02 -0500

    change project to routes instead of single page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Title from "./components/Title/Title";
 import Attacks from "./components/Attacks/Attacks";
 import Weaknesses from "./components/Weaknesses/Weaknesses";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
   state = {
@@ -11,31 +12,39 @@ class App extends Component {
     showWeaknesses: false,
   };
 
-  toggleShowAttacks = () => {
-    const newValue = !this.state.showAttacks;
-    this.setState({ showAttacks: newValue });
-  };
+  // toggleShowAttacks = () => {
+  //   const newValue = !this.state.showAttacks;
+  //   this.setState({ showAttacks: newValue });
+  // };
 
-  toggleShowWeaknesses = () => {
-    const newValue = !this.state.showWeaknesses;
-    this.setState({ showWeaknesses: newValue });
-  };
+  // toggleShowWeaknesses = () => {
+  //   const newValue = !this.state.showWeaknesses;
+  //   this.setState({ showWeaknesses: newValue });
+  // };
 
   render() {
-    let weaknesses = null;
-    if (this.state.showWeaknesses) {
-      weaknesses = <Weaknesses />;
-    }
+    // let weaknesses = null;
+    // if (this.state.showWeaknesses) {
+    //   weaknesses = <Weaknesses />;
+    // }
     return (
-      <div>
-        <Header
-          attacksClicked={this.toggleShowAttacks}
-          weaknessesClicked={this.toggleShowWeaknesses}
-        />
-        <Title />
-        {this.state.showAttacks ? <Attacks /> : null}
-        {weaknesses}
-      </div>
+      <Router>
+        <div>
+          <Header
+          // attacksClicked={this.toggleShowAttacks}
+          // weaknessesClicked={this.toggleShowWeaknesses}
+          />
+          <Title />
+          <Switch>
+            <Route exact path="/attacks">
+              <Attacks />
+            </Route>
+            <Route exact path="/weaknesses">
+              <Weaknesses />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,21 @@ import React from "react";
 // import Navbar from "react-bootstrap/Navbar";
 // import Nav from "react-bootstrap/Nav";
 import "./Header.css";
+import { BrowserRouter as Link } from "react-router-dom";
 
-const header = (props) => {
+const Header = (props) => {
+  // let { path, url } = useRouteMatch();
+
   return (
     <div className="header">
-      {/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">DRG WIKI</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link onClick={props.attacksClicked}>Attacks</Nav.Link>
-          <Nav.Link onClick={props.weaknessesClicked}>Weakneses</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar> */}
-      <button className="headerButton" onClick={props.attacksClicked}>
-        Attacks
+      <button className="headerButton">
+        <Link to={`/attacks`}>Attacks</Link>
       </button>
-      <button className="headerButton" onClick={props.weaknessesClicked}>
-        Weakneses
+      <button className="headerButton">
+        <Link to={`/weaknesses`}>Weaknesses</Link>
       </button>
     </div>
   );
 };
 
-export default header;
+export default Header;
